86c7d73e5474af2763ed6040b3fc9229a4154e5d
mdcruz <marie.cruz1@news.co.uk>
2020-05-05 21:57:33 +0100

    create sample tests to automate iframes

---
+++ README.md
@@ -0,0 +1,11 @@
+# Cypress IFrame Example
+
+An example repo showing how to automate iframes using Cypress.
+
+## Install dependencies
+
+Run `npm i`
+
+## Run Cypress tests
+
+Run `npx cypress run` or `npx cypress open`
--- cypress/integration/iframe.spec.js
+++ cypress/integration/iframe.spec.js
@@ -1,7 +1,30 @@
 /// <reference types="cypress" />
 
 describe('IFrames testing in Cypress', () => {
+  beforeEach(() => cy.visit('http://the-internet.herokuapp.com/iframe'));
+
   it('should let you switch to the iframe', () => {
-    cy.visit('index.html');
+    cy.getIframeDocument()
+      .clear()
+      .type('hello')
+      .type('{command}a')
+      .should('have.text', 'hello');
+    cy.get('#mceu_3').click();
+    cy.get('#mceu_9').click();
+    cy.getIframeDocument().find('ul li strong').should('have.text', 'hello');
+  });
+
+  it('should let you switch to the iframe using the cypress iframe plugin', () => {
+    cy.frameLoaded('#mce_0_ifr');
+    cy.iframe()
+      .clear()
+      .type('hello')
+      .type('{command}a')
+      .should('have.text', 'hello');
+    cy.get('#mceu_3').click();
+    cy.get('#mceu_9').click();
+
+    cy.frameLoaded('#mce_0_ifr');
+    cy.iframe().find('ul li strong').should('have.text', 'hello');
   });
 });
