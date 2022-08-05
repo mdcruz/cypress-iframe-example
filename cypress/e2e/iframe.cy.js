32a64beb4f5c3433f009551242d8f7be8c5579cf
mdcruz <marie.descruz@gmail.com>
2022-08-05 14:15:46 +0100

    migrate to cypress version 10

--- cypress/e2e/iframe.cy.js
+++ cypress/e2e/iframe.cy.js
@@ -5,31 +5,21 @@ describe('IFrames testing in Cypress', () => {
 
   beforeEach(() => {
     cy.visit('http://the-internet.herokuapp.com/iframe');
-    cy.get('#mceu_3').as('boldIcon');
-    cy.get('#mceu_9').as('listIcon');
   });
 
   it('should let you switch to the iframe', () => {
     cy.switchToIframe(iframe)
       .clear()
       .type('hello')
-      .type('{command}a')
       .should('have.text', 'hello');
-    cy.get('@boldIcon').click();
-    cy.get('@listIcon').click();
-    cy.switchToIframe(iframe).find('ul li strong').should('have.text', 'hello');
-  });
+
+    });
 
   it('should let you switch to the iframe using the cypress iframe plugin', () => {
     cy.frameLoaded(iframe);
     cy.iframe()
       .clear()
       .type('hello')
-      .type('{command}a')
       .should('have.text', 'hello');
-    cy.get('@boldIcon').click();
-    cy.get('@listIcon').click();
-
-    cy.iframe().find('ul li strong').should('have.text', 'hello');
   });
 });
--- cypress/integration/iframe-demo.spec.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="cypress" />
-
-describe('IFrames testing in Cypress', () => {
-  beforeEach(() => {
-    cy.visit('https://the-internet.herokuapp.com/iframe');
-  });
-});
